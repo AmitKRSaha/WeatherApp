ad155bcecfad7a3347e446712253d4b143856c90
LL\amisaha <amisaha@publicisgroupe.net>
2018-09-24 21:21:54 +0530

    Added Bootstrap and click handler

--- src/App.js
+++ src/App.js
@@ -6,15 +6,14 @@ import { Search } from './SearchComponent/Search';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App Container">
         <header className="App-header">
           <img src={sun} className="App-logo" alt="logo" />
           <h1 className="App-title">Get your Weather Details</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Search />
+        </header> 
+        <div className="container">   
+          <Search />
+        </div>
       </div>
     );
   }
--- src/SearchComponent/Search.js
+++ src/SearchComponent/Search.js
@@ -4,9 +4,25 @@ import React, {
 
 export class Search extends Component {
 
+    constructor() {
+        super();
+        this.searchWeatherreport = this.searchWeatherreport.bind(this);
+    }
+
+    searchWeatherreport() {
+        alert('OK');
+    }
+
     render() {
         return ( 
-            <div > From Search </div>
+            <div > 
+                <div className="input-group mb-3">
+                    <input type="text" className="form-control" placeholder="Search With City Name" aria-label="Search With City Name" aria-describedby="button-addon2"/>
+                    <div className="input-group-append">
+                        <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick= {this.searchWeatherreport}>Search</button>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
